2b5c9c69da9d031bfe6460cf14f822b3e557325b
jack price <jack.price@clock-work.co.uk>
2025-10-28 21:11:40 +0000

    feat: add brand colours

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
+use Filament\Support\Colors\Color;
 use Illuminate\Support\Facades\URL;
+use App\Support\Colors\ClockworkColor;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\Facades\FilamentColor;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +33,13 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        FilamentColor::register([
+            'gray' => ClockworkColor::Navy,
+            'info' => Color::Blue,
+            'primary' => ClockworkColor::Teal,
+            'success' => Color::Emerald,
+            'warning' => Color::Orange,
+        ]);
     }
 }
